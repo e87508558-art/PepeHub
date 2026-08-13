7cf0f00c923bb2f3be748b015e34bf98773608f8
e87508558-art <e87508558@gmail.com>
2026-08-13 20:19:10 +0700

    Create pepe.lua

---
+++ pepe.lua
@@ -0,0 +1,6 @@
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "PepeHub Loaded! 🐸",
+    Text = "Скрипт успешно запущен через твой GitHub!",
+    Duration = 5
+})
+print("PepeHub активирован!")
